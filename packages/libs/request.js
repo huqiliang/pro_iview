9352216d5416ffdf3d26e37084f8756187642778
胡奇良 <hql@ipms.cn>
2021-01-27 14:28:22 +0800

    feat:多种请求方式支持

--- packages/libs/request.js
+++ packages/libs/request.js
@@ -20,6 +20,8 @@ function pick({ method, datas, keys }) {
 }
 
 export default ({ request, method, keys, datas }) => {
+  console.log(request);
+  console.log(_.isObjectLike(request));
   // 字符串 是request:"/url"
   if (_.isString(request)) {
     return axios({ url: request, method, ...pick({ method, keys, datas }) });
@@ -27,7 +29,11 @@ export default ({ request, method, keys, datas }) => {
   // 对象 是request:{url:"/url"}
   else if (_.isObjectLike(request)) {
     const keys = _.has(request, "keys") ? request.keys : keys;
+
     if (_.has(request, "request") && !_.has(request, "url")) {
+      if (_.isFunction(request.request)) {
+        return request.request(datas);
+      }
       request.url = request.request;
     }
     const method = method ? method : request.method;
@@ -35,6 +41,7 @@ export default ({ request, method, keys, datas }) => {
   }
   // 对象本身就是axios
   else if (_.isFunction(request)) {
+    console.log(request);
     return request(datas);
   }
   return new Promise(resolve => {
