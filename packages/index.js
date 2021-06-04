8049f792d28654d963db04b7441e1ade109a854b
胡奇良 <hql@ipms.cn>
2021-06-04 18:02:01 +0800

    feat:支持传入http

--- packages/index.js
+++ packages/index.js
@@ -1,13 +1,18 @@
 import components from "./components";
 import { version } from "../package.json";
 import locale from "./locale";
+import axios from "axios";
 
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = (Vue, opts = {}) => {
+  if (opts.http) {
+    opts.http(axios);
+  }
   // 判断是否安装
   if (install.installed) return;
   locale.use(opts.locale);
   locale.i18n(opts.i18n);
+
   // 遍历注册全局组件
   components.map(component => {
     if (component.name) {
