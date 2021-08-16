55f8ebf58b1cacc6faee7caaa6912cd1f99498fb
胡奇良 <hql@ipms.cn>
2021-08-16 15:23:33 +0800

    feat:文档更新

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  title: "iview高阶组件库", // 设置网站标题
+  description: "iview高阶组件库",
+  displayAllHeaders: true, //显示所有页面的标题链接
+  base: "/pro_iview/",
+  head: [],
+  plugins: [],
+  themeConfig: {
+    // 添加导航栏
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "指南", link: "/guide/" },
+      { text: "更新日志", link: "/logs/" },
+      {
+        text: "github",
+        link: "https://github.com/huqiliang/pro_iview"
+      }
+    ],
+    sidebar: {
+      sidebarDepth: 3,
+      "/guide/": [
+        {
+          title: "架构设计", // 必要的
+          collapsable: false,
+          children: [
+            {
+              title: "组件总览",
+              path: "/guide/all/preview"
+            }
+          ]
+        },
+        {
+          title: "数据展示",
+          collapsable: false,
+          children: [
+            {
+              title: "表格生成",
+              path: "/guide/data/config"
+            }
+          ]
+        },
+        {
+          title: "天工手册",
+          collapsable: false,
+          children: [
+            {
+              title: "简介",
+              path: "/guide/server/intro"
+            },
+            {
+              title: "项目配置",
+              path: "/guide/server/product"
+            },
+            {
+              title: "页面配置",
+              path: "/guide/server/page"
+            }
+          ]
+        }
+      ],
+      "/logs/": [
+        {
+          title: "更新日志",
+          children: [
+            {
+              title: "更新说明", // 必要的
+              path: "/logs/logs" // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            }
+          ] // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        }
+      ]
+    },
+    sidebarDepth: 1, //左侧导航显示的层级
+    lastUpdated: "Last Updated"
+  }
+};
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroText: Pro Iview
+tagline: 让中后台开发更简单
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 简单易用
+  details: 在 Iview 上进行了自己的封装，更加易用。
+- title: View Design
+  details: 与 View Design 设计体系一脉相承，无缝对接 antd 项目。
+- title: 国际化
+  details: 提供完备的国际化，与 View Design 体系打通。
+
+
+footer: MIT Licensed | Copyright © 2018-present Teemo
+---
