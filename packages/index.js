632de755a8af7ce165417916771ec567b8a9e51a
胡奇良 <hql@ipms.cn>
2021-09-17 10:52:16 +0800

    feat:去掉新的语法

--- packages/index.js
+++ packages/index.js
@@ -6,9 +6,9 @@ import { success } from "./libs/request";
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = (Vue, opts = {}) => {
   global.$http = Vue.$axios = Vue.prototype.$http =
-    opts?.http?.axios || opts.http || axios;
+    (opts.http && opts.http.axios) || opts.http || axios;
   global.$success = Vue.$success = Vue.prototype.$success =
-    opts?.http?.success || success;
+    (opts.http && opts.http.success) || success;
 
   // Vue.prototype.$http = opts.axios;
   // 判断是否安装
