89043dc30662f83a535ba474e128db3124dbd1d6
胡奇良 <hql@ipms.cn>
2022-02-23 14:18:01 +0800

    feat:删除传入success

--- packages/index.js
+++ packages/index.js
@@ -2,14 +2,10 @@ import components from "./components";
 import { version } from "../package.json";
 import locale from "./locale";
 import axios from "axios";
-import { success } from "./libs/request";
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = (Vue, opts = {}) => {
   global.$http = Vue.$axios = Vue.prototype.$http =
     (opts.http && opts.http.axios) || opts.http || axios;
-  global.$success = Vue.$success = Vue.prototype.$success =
-    (opts.http && opts.http.success) || success;
-
   // Vue.prototype.$http = opts.axios;
   // 判断是否安装
   if (install.installed) return;
