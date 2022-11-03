e906d0cc402dfe89bd2e4f4675080cb20818f89f
胡奇良 <hql@ipms.cn>
2022-11-03 12:11:54 +0800

    feat: 自定义渲染示例

--- examples/views/ProTable.vue
+++ examples/views/ProTable.vue
@@ -8,7 +8,7 @@ export default {
     return {
       config: {
         toolBarActions: ["new", "refresh", "fullscreen", "rowSetting"],
-        request: "https://jsonplaceholder.typicode.com/posts",
+        request: "http://192.168.0.38:3000/mock/106/api/table",
         columns: [
           {
             title: "userId",
@@ -27,10 +27,52 @@ export default {
             ]
           },
           { title: "id", key: "id" },
-          { title: "title", key: "title" },
-          { title: "body", key: "body" }
+          { title: "body", key: "body" },
+          {
+            minWidth: 90,
+            formWidth: 20,
+            align: "center",
+            title: "状态",
+            key: "createUser8",
+            renderTable: {
+              type: "i-switch",
+              props: {
+                disabled: true,
+                "true-value": "T",
+                "false-value": "F"
+              }
+            },
+            // renderSearch: { type: "Select", props: {} },
+            renderSearch: ({ value, input }) => {
+              return (
+                <Select value={value} oninput={input} clearable>
+                  <Option value="F">开</Option>
+                  <Option value="T">关</Option>
+                </Select>
+              );
+            },
+            renderForm: ({ value, input }) => {
+              return (
+                <i-switch value={value} oninput={input} clearable>
+                  <span value="F" slot="open">
+                    开
+                  </span>
+                  <span value="T" slot="close">
+                    关
+                  </span>
+                </i-switch>
+              );
+            }
+          }
         ],
-        map: {}
+        map: {
+          current: "firstResult",
+          pageSize: "page",
+          dataPath: "data",
+          totalPath: "totalRows",
+          message: "message"
+          // success: "code===3004"
+        }
       }
     };
   }
